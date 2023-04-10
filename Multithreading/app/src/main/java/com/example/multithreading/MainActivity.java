ac88e3317de44dfbe401acacfb4de15be5855590
innaanikina <avril-avril@mail.ru>
2023-04-11 00:28:45 +0500

    Использование AsyncTask во фрагменте

--- Multithreading/app/src/main/java/com/example/multithreading/MainActivity.java
+++ Multithreading/app/src/main/java/com/example/multithreading/MainActivity.java
@@ -2,77 +2,14 @@ package com.example.multithreading;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    int[] integers = null;
-    int clicks = 0;
-    ProgressBar indicatorBar;
-    TextView statusView;
-    TextView clicksView;
-    Button progressBtn;
-    Button clicksBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        integers = new int[100];
-        for (int i = 0; i < 100; i++) {
-            integers[i] = i + 1;
-        }
-
-        indicatorBar = (ProgressBar) findViewById(R.id.indicator);
-        statusView = findViewById(R.id.statusView);
-        progressBtn = findViewById(R.id.progressBtn);
-
-        progressBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new ProgressTask().execute();
-            }
-        });
-
-        clicksView = findViewById(R.id.clicksView);
-        clicksBtn = findViewById(R.id.clicksBtn);
-        clicksBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clicks++;
-                clicksView.setText("Clicks: " + clicks);
-            }
-        });
-    }
-
-    class ProgressTask extends AsyncTask<Void, Integer, Void> {
-        @Override
-        protected Void doInBackground(Void... voids) {
-            for (int i = 0; i < integers.length; i++) {
-                publishProgress(i);
-                SystemClock.sleep(400);
-            }
-            return (null);
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            indicatorBar.setProgress(values[0] + 1);
-            statusView.setText("Статус: " + String.valueOf(values[0] + 1));
-        }
-
-        @Override
-        protected void onPostExecute(Void unused) {
-            Toast.makeText(getApplicationContext(), "Задача завершена", Toast.LENGTH_SHORT).show();
-        }
     }
 }
 
--- Multithreading/app/src/main/java/com/example/multithreading/ProgressFragment.java
+++ Multithreading/app/src/main/java/com/example/multithreading/ProgressFragment.java
@@ -1,12 +1,15 @@
 package com.example.multithreading;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,23 +17,58 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 public class ProgressFragment extends Fragment {
+    int[] integers = null;
+    ProgressBar indicatorBar;
+    TextView statusView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_progress, container, false);
 
-        ProgressBar indicatorBar = (ProgressBar) view.findViewById(R.id.indicator);
-        TextView statusView = (TextView) view.findViewById(R.id.statusView);
+        integers = new int[100];
+        for (int i = 0; i < 100; i++) {
+            integers[i] = i + 1;
+        }
+
+        indicatorBar = (ProgressBar) view.findViewById(R.id.indicator);
+        statusView = (TextView) view.findViewById(R.id.statusView);
         Button btnFetch = (Button) view.findViewById(R.id.progressBtn);
 
-        MyViewModel model = new ViewModelProvider(requireActivity()).get(MyViewModel.class);
-
-        model.getValue().observe(getViewLifecycleOwner(), value -> {
-            indicatorBar.setProgress(value);
-            statusView.setText("Статус: " + value);
+        btnFetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ProgressTask().execute();
+            }
         });
-
-        btnFetch.setOnClickListener(v -> model.execute());
         return view;
+    }
+
+    class ProgressTask extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            for (int i = 0; i < integers.length; i++) {
+                publishProgress(i);
+                SystemClock.sleep(400);
+            }
+            return (null);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            indicatorBar.setProgress(values[0] + 1);
+            statusView.setText("Статус: " + String.valueOf(values[0] + 1));
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            Toast.makeText(getActivity(), "Задача завершена", Toast.LENGTH_SHORT).show();
+        }
     }
 }
