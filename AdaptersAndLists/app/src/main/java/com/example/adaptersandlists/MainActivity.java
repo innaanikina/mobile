0aa0d19736b94574b83103fb65a599e8a5b8847d
innaanikina <avril-avril@mail.ru>
2023-04-17 19:58:03 +0500

    Дз 7. Multiple choice

--- AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
+++ AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
@@ -3,13 +3,42 @@ package com.example.adaptersandlists;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] countries = getResources().getStringArray(R.array.countries);
+
+        TextView selection = findViewById(R.id.selection);
+        ListView countriesList = findViewById(R.id.countriesList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_multiple_choice, countries);
+        countriesList.setAdapter(adapter);
+
+        countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                SparseBooleanArray selected = countriesList.getCheckedItemPositions();
+
+                String selectedItems = "";
+
+                for (int j = 0; j < countries.length; j++) {
+                    if (selected.get(j)) {
+                        selectedItems += countries[j] + ", ";
+                    }
+                }
+                selection.setText("Выбрано: " + selectedItems);
+            }
+        });
     }
 }
