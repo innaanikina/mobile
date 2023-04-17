38c3762f0396c5327ca5d9fa024e32d212198e2d
innaanikina <avril-avril@mail.ru>
2023-04-17 23:20:03 +0500

    Дз 7. Выпадающий список Spinner

--- AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
+++ AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
@@ -3,29 +3,43 @@ package com.example.adaptersandlists;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] countries = {"Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Product> products = new ArrayList<>();
+        TextView selection = findViewById(R.id.selection);
 
-        if (products.size() == 0) {
-            products.add(new Product("Картофель", "кг."));
-            products.add(new Product("Чай", "шт."));
-            products.add(new Product("Яйца", "шт."));
-            products.add(new Product("Молоко", "л."));
-            products.add(new Product("Макароны", "кг."));
-        }
-        ListView productList = findViewById(R.id.productList);
-        ProductAdapter adapter = new ProductAdapter(this, R.layout.list_item, products);
-        productList.setAdapter(adapter);
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, countries);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = (String) adapterView.getItemAtPosition(i);
+                selection.setText(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        };
+        spinner.setOnItemSelectedListener(itemSelectedListener);
     }
 
 }
