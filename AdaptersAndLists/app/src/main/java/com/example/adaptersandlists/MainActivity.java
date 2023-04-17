a5e944b97599fbad7bd1f684f9439bcac3d73994
innaanikina <avril-avril@mail.ru>
2023-04-17 21:54:16 +0500

    Дз 7. Список продуктов

--- AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
+++ AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
@@ -3,43 +3,29 @@ package com.example.adaptersandlists;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<State> states = new ArrayList<>();
-    ListView countriesList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setInitialData();
-        countriesList = findViewById(R.id.countriesList);
-        StateAdapter stateAdapter = new StateAdapter(this, R.layout.list_item, states);
-        countriesList.setAdapter(stateAdapter);
+        ArrayList<Product> products = new ArrayList<>();
 
-        AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                State selectedState = (State) adapterView.getItemAtPosition(i);
-                Toast.makeText(getApplicationContext(),
-                        "Был выбран пункт " + selectedState.getName(),
-                        Toast.LENGTH_SHORT).show();
-            }
-        };
-        countriesList.setOnItemClickListener(itemListener);
+        if (products.size() == 0) {
+            products.add(new Product("Картофель", "кг."));
+            products.add(new Product("Чай", "шт."));
+            products.add(new Product("Яйца", "шт."));
+            products.add(new Product("Молоко", "л."));
+            products.add(new Product("Макароны", "кг."));
+        }
+        ListView productList = findViewById(R.id.productList);
+        ProductAdapter adapter = new ProductAdapter(this, R.layout.list_item, products);
+        productList.setAdapter(adapter);
     }
 
-    private void setInitialData() {
-        states.add(new State("Бразилия", "Бразилиа", R.drawable.brazil));
-        states.add(new State("Аргентина", "Буэнос-Айрес", R.drawable.argentina));
-        states.add(new State("Колумбия", "Богота", R.drawable.colombia));
-        states.add(new State("Уругвай", "Монтевидео", R.drawable.uruguay));
-        states.add(new State("Чили", "Сантьяго", R.drawable.chile));
-    }
 }
