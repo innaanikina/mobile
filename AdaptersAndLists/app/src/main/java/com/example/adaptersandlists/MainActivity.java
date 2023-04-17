35467a5f3fcd981e014faf51adff7cfb85f99f67
innaanikina <avril-avril@mail.ru>
2023-04-17 19:42:27 +0500

    Дз 7. String-array (из кода активити).

--- AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
+++ AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
@@ -7,15 +7,14 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
-    String[] countries = {"Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ListView countriesList = findViewById(R.id.countriesList);
+
+        String[] countries = getResources().getStringArray(R.array.countries);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, countries);
