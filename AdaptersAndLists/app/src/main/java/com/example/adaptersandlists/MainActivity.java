facbc7f62d6ad9acdc5824bc3bacafd474470e2f
innaanikina <avril-avril@mail.ru>
2023-04-17 20:20:25 +0500

    Дз 7. Добавление и удаление элементов в список.

--- AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
+++ AdaptersAndLists/app/src/main/java/com/example/adaptersandlists/MainActivity.java
@@ -3,42 +3,61 @@ package com.example.adaptersandlists;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> users = new ArrayList<>();
+    ArrayList<String> selectedUsers = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+    ListView usersList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] countries = getResources().getStringArray(R.array.countries);
+        Collections.addAll(users, "Tom", "Bob", "Sam", "Alice");
+        usersList = findViewById(R.id.usersList);
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_multiple_choice, users);
+        usersList.setAdapter(adapter);
 
-        TextView selection = findViewById(R.id.selection);
-        ListView countriesList = findViewById(R.id.countriesList);
+        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_multiple_choice, countries);
-        countriesList.setAdapter(adapter);
-
-        countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                SparseBooleanArray selected = countriesList.getCheckedItemPositions();
-
-                String selectedItems = "";
-
-                for (int j = 0; j < countries.length; j++) {
-                    if (selected.get(j)) {
-                        selectedItems += countries[j] + ", ";
-                    }
+                String user = adapter.getItem(i);
+                if (usersList.isItemChecked(i)) {
+                    selectedUsers.add(user);
+                } else {
+                    selectedUsers.remove(user);
                 }
-                selection.setText("Выбрано: " + selectedItems);
             }
         });
+    }
+
+    public void add(View view) {
+        EditText userName = findViewById(R.id.userName);
+        String user = userName.getText().toString();
+        if (!user.isEmpty()) {
+            adapter.add(user);
+            userName.setText("");
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    public void remove(View view) {
+        for (int i = 0; i < selectedUsers.size(); i++) {
+            adapter.remove(selectedUsers.get(i));
+        }
+        usersList.clearChoices();
+        selectedUsers.clear();
+        adapter.notifyDataSetChanged();
     }
 }
